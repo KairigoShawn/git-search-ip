f700928b00d83b8d6105b57d8a4797157006ab23
KairigoShawn <kairigonjogu@outlook.com>
2021-10-21 13:35:12 +0300

    fixes some bugs in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProfileService } from './profile.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'git-search';
+
+  constructor() {}
+
+
 }
