b21815cdae6f5371a373648bb613dabd1ff78498
KairigoShawn <kairigonjogu@outlook.com>
2021-10-22 01:41:17 +0300

    rectifies minor errors in environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  token: Buffer.from('dG9rZW4gZ2hwX1loOXk2Y1FjYzVWMHhpUmE4bW1ySTIya0J0WmFuZjA2UG8xZA==', 'base64').toString(),
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ import { Buffer } from "buffer";
 
 export const environment = {
   production: false,
-  apiUrl : 'https://api.github.com',
-  token : Buffer.from('dG9rZW4gZ2hwX1loOXk2Y1FjYzVWMHhpUmE4bW1ySTIya0J0WmFuZjA2UG8xZA==', 'base64').toString(),
+  apiUrl: 'https://api.github.com',
+  token: Buffer.from('dG9rZW4gZ2hwX1loOXk2Y1FjYzVWMHhpUmE4bW1ySTIya0J0WmFuZjA2UG8xZA==', 'base64').toString(),
 };
 
 /*
