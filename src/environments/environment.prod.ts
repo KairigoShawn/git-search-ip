934339810754db7b33994984e434d964dc06b1dd
KairigoShawn <kairigonjogu@outlook.com>
2021-10-22 01:43:05 +0300

    final commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
+import { Buffer } from "buffer";
+
 export const environment = {
   production: true,
   token: Buffer.from('dG9rZW4gZ2hwX1loOXk2Y1FjYzVWMHhpUmE4bW1ySTIya0J0WmFuZjA2UG8xZA==', 'base64').toString(),
